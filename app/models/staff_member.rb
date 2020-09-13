c94a76e9e8db78408212811b4d9e7cc267aba4d1
gakitomo <tmo8002@gmail.com>
2020-09-13 14:38:58 +0900

    Update staff_member.rb

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -13,7 +13,7 @@ include StringNormalizer
 
   KATAKANA_REGEXP = /A\[\p{katakana}\u{30fc}]+\z/
 
-  validates :email, presence: true, "valid_email_2/email":true, uniqueless: {case_sensitive: false}
+  validates :email, presence: true, "valid_email_2/email":true, uniqueness: {case_sensitive: false}
   validates :family_name, :given_name, presence: true
   validates :family_name_kana, :given_name_kana, presence: true, format:{with: KATAKANA_REGEXP, allow_blank: true}
   validates :start_date, presence: true, date:{
