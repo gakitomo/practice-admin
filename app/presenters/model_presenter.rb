88c632f5c1d495f9987d8246a193ca0de6c9022b
gakitomo <tmo8002@gmail.com>
2020-09-14 10:03:04 +0900

    Update model_presenter.rb

--- app/presenters/model_presenter.rb
+++ app/presenters/model_presenter.rb
@@ -1,7 +1,8 @@
 class ModelPresenter
   attr_reader :object, :view_context
+  delegate :raw, to: :view_context
 
-  def initialize
+  def initialize(object, view_context)
     @object = object
     @view_context = view_context
   end
