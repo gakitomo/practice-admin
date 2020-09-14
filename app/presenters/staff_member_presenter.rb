2206f9a8ce8d064305c2296c9d6e55b8dd097c34
gakitomo <tmo8002@gmail.com>
2020-09-14 09:56:29 +0900

    Update staff_member_presenter.rb

--- app/presenters/staff_member_presenter.rb
+++ app/presenters/staff_member_presenter.rb
@@ -1,2 +1,10 @@
 class StaffMemberPresenter < ModelPresenter
+  # 職員の停止フラグのon/offを表現する記号を返す。
+  # ON: BALLOTBOX WITH CHECK(U+2611)
+  # OFF: BALLOT BOX(U+2610)
+  def suspended_mark
+    object.suspended? ?
+      view_context.raw("&#x2611;"):
+      view_context.raw("&#x2610;")
+  end
 end
