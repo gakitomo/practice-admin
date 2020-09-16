be14faa75de8ee4322568a5868a12bb6741197ec
gakitomo <tmo8002@gmail.com>
2020-09-16 18:08:44 +0900

    Update Presenter

--- app/presenters/staff_event_presenter.rb
+++ app/presenters/staff_event_presenter.rb
@@ -3,14 +3,15 @@ class StaffEventPresenter < ModelPresenter
 
   def table_row
     markup(:tr) do |m|
-      unless view_context. instance_variable_get(:@staff_member)
+      unless view_context.instance_variable_get(:@staff_member)
         m.td do
-          m << link_to(member.family_name + member.given_name,[:admin, member, :staff_events])
+          m << link_to(member.family_name + member.given_name,
+            [:admin, member, :staff_events])
         end
       end
       m.td description
       m.td(:class => "date") do
-        m.text occurred_at.strftime("%Y/%m/%d/ %H:%M:%S")
+        m.text occurred_at.strftime("%Y/%m/%d %H:%M:%S")
       end
     end
   end
