2c57df386cf21d90aa180ef6e108963dbe7deb6d
gakitomo <tmo8002@gmail.com>
2020-10-06 20:53:36 +0900

    Update customer_form.rb

--- app/forms/staff/customer_form.rb
+++ app/forms/staff/customer_form.rb
@@ -1,12 +1,11 @@
 class Staff::CustomerForm
   include Activemodel::model
-    attr_accessor :customer
+  attr_accessor :customer
 
-    def initialize(customer = nil)
-      @customer = customer
-      @customer || = Customer.new(gender: "male")
-      @customer.build_home_address unless @customer.home_address
-      @customer.build_home_address unless @customer.work_address
-    end
+  def initialize(customer = nil)
+    @customer = customer
+    @customer ||= Customer.new(gender: "male")
+    @customer.build_home_address unless @customer.home_address
+    @customer.build_home_address unless @customer.work_address
   end
 end
