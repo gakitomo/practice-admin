881980b0388f924d93a26d6566b56061bc1b03fd
gakitomo <tmo8002@gmail.com>
2020-10-06 20:31:10 +0900

    Update user_form_presenter.rb

--- app/presenters/user_form_presenter.rb
+++ app/presenters/user_form_presenter.rb
@@ -15,10 +15,4 @@ class UserFormPresenter < FormPresenter
     end
   end
 
-  def suspended_check_box
-    markup(:div, class:"check-boxes") do |m|
-      m << check_box(:suspended)
-      m << label(:suspended, "アカウント停止")
-    end
-  end
 end
