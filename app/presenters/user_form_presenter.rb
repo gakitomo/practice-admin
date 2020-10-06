e3eca5502937b57b6a64b6b0584d93d587685f20
gakitomo <tmo8002@gmail.com>
2020-10-06 20:30:12 +0900

    rename user_form_presenter.rb

--- app/presenters/user_form_presenter.rb
+++ app/presenters/user_form_presenter.rb
@@ -1,4 +1,4 @@
-class StaffMemberFormPresenter < FormPresenter
+class UserFormPresenter < FormPresenter
   def password_field_block(name, label_text, options = {})
     if object.new_record?
       super(name, label_text, options)
