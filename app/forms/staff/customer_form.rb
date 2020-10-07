0ca984cfa26f23436804da35c5d2067c55c67745
gakitomo <tmo8002@gmail.com>
2020-10-07 21:47:55 +0900

    Create Customer_Views

--- app/forms/staff/customer_form.rb
+++ app/forms/staff/customer_form.rb
@@ -1,11 +1,13 @@
 class Staff::CustomerForm
   include Activemodel::model
+
   attr_accessor :customer
+  delegate :persisted?, to: :customer
 
   def initialize(customer = nil)
     @customer = customer
     @customer ||= Customer.new(gender: "male")
     @customer.build_home_address unless @customer.home_address
-    @customer.build_home_address unless @customer.work_address
+    @customer.build_work_address unless @customer.work_address
   end
 end
